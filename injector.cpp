75b869fec0a6f3ca973695ed8920c892fd8ffc1a
Juliano Franz <jufranz@gmail.com>
2014-08-04 20:23:49 -0300

    Added game launcher
    Added the RS launcher inside the Injector. If the game is not launched
    within 20 seconds the injector aborts.

--- injector.cpp
+++ injector.cpp
@@ -10,44 +10,53 @@ int main()
 	//Search for the RS window
 	hwnd = FindWindow(NULL, L"Rocksmith 2014");
 	
-	if (!hwnd)
-	{	
-		cout << "Window not found!\n";
+	system("start steam://rungameid/221680");
+	hwnd = FindWindow(NULL, L"Rocksmith 2014");
+
+	while (!hwnd)
+	{
+		tries++;
+		Sleep(2000);
+		if (tries >= 10)
+		{
+			cout << "Rocksmith not found! \n Make sure everything is ok!\n";
+			cin.get();
+			exit(1);
+		}
+		hwnd = FindWindow(NULL, L"Rocksmith 2014");
+	}
+	
+	GetWindowThreadProcessId(hwnd, &pid);
+	//Get the appropriate PID
+	HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
+	if (!phandle)
+	{
+		cout << "Could not get handle!\n";
 		cin.get();
 	}
 	else
 	{
-		GetWindowThreadProcessId(hwnd, &pid);
-		//Get the appropriate PID
-		HANDLE phandle = OpenProcess(PROCESS_ALL_ACCESS, 0, pid);
-		if (!phandle)
-		{
-			cout << "Could not get handle!\n";
-			cin.get();
-		}
-		else
-		{
-			//55 8B EC 83 EC 08 8B 47 14 <-- ORIGINAL!
-			//B0 01 C3 90 EC 08 RB 47 17 <-- HACKED!
+		//55 8B EC 83 EC 08 8B 47 14 <-- ORIGINAL!
+		//B0 01 C3 90 EC 08 RB 47 17 <-- HACKED!
 			
-			cout << "Ok, now let's try that signature stuff... \n";
-			DWORD sigAddr = 0x0054B5C0;	//Initial RAM Address TODO: Search for the HEX string
-			byte sig[4] = { 0xB0, 0X01, 0XC3, 0X90 };
-			DWORD newdatasize = sizeof(sig[0]);
+		cout << "Ok, now let's try that signature stuff... \n";
+		DWORD sigAddr = 0x0054B5C0;	//Initial RAM Address TODO: Search for the HEX string
+		byte sig[4] = { 0xB0, 0X01, 0XC3, 0X90 };
+		DWORD newdatasize = sizeof(sig[0]);
 
-			for (int i = 0; i < 4; i++)
+		for (int i = 0; i < 4; i++)
+		{
+			if (WriteProcessMemory(phandle, (void*)(sigAddr + i), &sig[i], newdatasize, NULL))
+				cout << "GOOD SIGN " << i << endl;
+			else
 			{
-				if (WriteProcessMemory(phandle, (void*)(sigAddr + i), &sig[i], newdatasize, NULL))
-					cout << "GOOD SIGN " << i << endl;
-				else
-				{
-					cout << "Frak! Something went wrong! \n";
-					break;
-				}
+				cout << "Frak! Something went wrong! \n";
+				break;
 			}
-
-			system("pause");
-			return 0;
 		}
+
+		system("pause");
+		return 0;
+		
 	}
 }
